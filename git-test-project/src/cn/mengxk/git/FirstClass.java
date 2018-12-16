17d224fa5bd928186c2b41a51149f38c4b47f490
mengxiangkun <1040985829@qq.com>
2018-12-16 21:00:23 +0800

    把本地工程添加到git

---
+++ git-test-project/src/cn/mengxk/git/FirstClass.java
@@ -0,0 +1,17 @@
+package cn.mengxk.git;
+
+/**
+ * @auther Mengxiangkun
+ * @date 2018/12/16 20:54
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
